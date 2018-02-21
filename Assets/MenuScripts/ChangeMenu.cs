3026f78a9222488b48321891beeafeab5518477f
molkiewicz <Borgitek@gmail.com>
2018-02-22 00:10:25 +0100

    PlaceHolderGrpahics
    P1

---
+++ Assets/MenuScripts/ChangeMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeMenu : MonoBehaviour
+{
+    public string SceneToGoName = "";
+    public void Start()
+    {
+        Debug.Log(this.transform.parent.name);
+        if (string.IsNullOrEmpty(SceneToGoName))
+        {
+            SceneToGoName = this.transform.parent.name;
+        }
+    }
+    public void OnMouseClick()
+    {
+
+        SceneManager.LoadScene(SceneToGoName);
+    }
+}
